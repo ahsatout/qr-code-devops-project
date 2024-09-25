54e120ec0b5c44078bb1bdfcf36b0ce1f476b6af
ABDELHAKIM SATOUT <hakim.satout@gmail.com>
2024-09-25 16:45:09 +0100

    Fix a typo

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "tf_state_lock" {
-    name         = "terraform-state-lock"
+    name         = "state-lock"
     write_capacity = 1
     read_capacity = 1
     hash_key     = "LockID"
