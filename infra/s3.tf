eefbc9150523b16a8726cc12ce9d665e0b8b4e9a
ABDELHAKIM SATOUT <hakim.satout@gmail.com>
2024-09-26 20:12:25 +0100

    Update s3.tf to Make QR codes bucket publicly accessible

--- infra/s3.tf
+++ infra/s3.tf
@@ -12,3 +12,46 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.buckets["QR"].id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership_controls" {
+  bucket = aws_s3_bucket.buckets["QR"].id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access,
+    aws_s3_bucket_ownership_controls.ownership_controls,
+  ]
+
+  bucket = aws_s3_bucket.buckets["QR"].id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.buckets["QR"].id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.buckets["QR"].arn}/*"
+      },
+    ]
+  })
+}
